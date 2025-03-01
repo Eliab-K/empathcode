af663d34e224820fce70c92bad2f93c7f5c19704
Amanyire Cindy <amanyrecindy@gmail.com>
2025-03-01 23:11:38 +0300

    added a new feature

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from app.model import ContrastiveModel, extract_features
 from fastapi import FastAPI, File, UploadFile, HTTPException, Request
 from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
