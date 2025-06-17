39c89b470b7e27a8136a44364121133b94e54f04
AmanyireCI <amanyirecindy@gmail.com>
2025-06-17 09:56:06 +0300

    deleted code

--- app/__init__.py
+++ app/__init__.py
@@ -8,13 +8,6 @@ from starlette.middleware.sessions import SessionMiddleware
 
 app = FastAPI()
 
-# Mount all routes from main.py
-<<<<<<< HEAD
-app.include_router(router)
-=======
-#app.include_router(router)
->>>>>>> master
-
 # Enable session management (for login)
 app.add_middleware(SessionMiddleware, secret_key="supersecret")  
 
